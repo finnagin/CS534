51bece13910479b6288cbf3b914cf898136f09da
mlowell28 <49284072+mlowell28@users.noreply.github.com>
2019-10-17 16:39:19 -0700

    Corrected algorithm, tested using synthetic data
    This function should be complete at this time, vectorization implementation would be good but I wanted to get something working as fast as possible.

--- I1/Gradient_Descent.py
+++ I1/Gradient_Descent.py
@@ -9,17 +9,14 @@ import numpy as np
 
 # this class runs gradient descent for linear regression, it accepts a target training vector along with a training features matrix where each row represents a single example,
 # additionally it requires a learning rate, regularization constant and two stopping criteria, norm on the gradient and a maximum number of iterations
-# which ever is reached first terminates the descent
+# which ever is reached first terminates the descent,
 
 class Gradient_Descent:
     
-    def run_gradient_descent(self,target_training_vector, training_features_matrix, starting_weights, learning_rate, regularization_constant, gradient_norm_stopping_criteria, max_iterations):
+    def run_gradient_descent(self, y, X, w_0, alpha, lambda_0, epsilon, max_iter):
         
-        number_of_training_examples = target_training_vector.size
-        number_of_features = starting_weights.size
-        
-        weight_vector = starting_weights
-        weight_vector_gradient = np.zeros(number_of_features)
+        number_of_training_examples = y.size
+        number_of_features = w_0.size
         
         
         training_iteration = 0 
@@ -27,86 +24,73 @@ class Gradient_Descent:
         
         # calculuate sum of squared errors for initial starting weight
         
-        training_vector_index = 0
-        Sum_of_Squared_Errors = []
-        norm_of_weight_vector_gradient = []
+        w_grad_norms = []
         
     
         # apply gradient descent until max iteration or stopping criteria is met
+       
+        w = w_0
+        w_vecs = []
+        w_grad_vecs = []
+        w_vecs.append(w_0)
         
-        while(training_iteration < max_iterations and np.linalg.norm(weight_vector_gradient) >= gradient_norm_stopping_criteria): 
-            
-            # calculate Sum of Squares Errors for current weights  
-        
-            Sum_of_Squared_Errors_Current_Weight = 0
-            training_vector_index = 0
-            
-            # using fact python has starting index of 0
-            
-            while(training_vector_index < number_of_training_examples):
-                Sum_of_Squared_Errors_Current_Weight = Sum_of_Squared_Errors_Current_Weight + (target_training_vector[training_vector_index]-np.dot(weight_vector, training_features_matrix[training_vector_index,:]))^2
-                training_vector_index = training_vector_index + 1
-            
-            # append to list of sum of squared errors 
-            
-            Sum_of_Squared_Errors.append(Sum_of_Squared_Errors_Current_Weight)
+        while(1): 
             
             
-           # compute gradient by computing partial derivative of each weight and updating weight_vector_gradient
+           # compute gradient by computing partial derivative of each weight and updating weight_grad
          
-            weight_index = 0;
+            w_index = 0;
         
             # use the fact python has starting index of 0
             
-            while(weight_index < number_of_features):
+            w_grad = np.zeros(number_of_features)
             
-                training_vector_index = 0;
+            
+            while(w_index < number_of_features):
+            
+                y_index = 0;
                 
                 # use fact python has starting index of 0
         
-                while(training_vector_index < number_of_training_examples):
+                while(y_index < number_of_training_examples):
                     
                     # apply partial derivative formula summing up across all training examples
                     
-                    weight_vector_gradient[weight_index] = weight_vector_gradient[weight_index] + 2*(target_training_vector[training_vector_index]- np.dot(weight_vector, training_features_matrix[training_vector_index,:]))*training_features_matrix[training_vector_index,weight_index]
-                    training_vector_index = training_vector_index + 1
+                    w_grad[w_index] = w_grad[w_index] + (-1)*2*(y[y_index]- np.dot(w, X[y_index,:]))*X[y_index,w_index]
+                    y_index = y_index + 1
                     
-                # apply weight regularization affect to partial derivative 
+                # apply weight regualization effect to partial excluding bias term
                 
-                weight_vector_gradient[weight_index] = weight_vector_gradient[weight_index] + regularization_constant*2* weight_vector[weight_index]
+                if(w_index > 0):
+                    w_grad[w_index] = w_grad[w_index] + lambda_0*2* w[w_index]
                 
-                weight_index = weight_index + 1
+                w_index = w_index + 1
         
-            # update append norm of current weight vector gradient to list of norms, 
+            # append current gradient vector to list
             
-            norm_of_weight_vector_gradient.append(np.linalg.norm(weight_vector_gradient))
+            w_grad_vecs.append(w_grad)
+            
+            # append norm of gradient to array
+            
+            w_grad_norms.append(np.linalg.norm(w_grad))
+            
+            # exit loop if done
+            
+            if(training_iteration > max_iter or np.linalg.norm(w_grad) < epsilon):
+                
+                return [w_vecs, w_grad_vecs, w_grad_norms]
+            
             
             # update weight vector using learning rate 
         
-            weight_vector = weight_vector - learning_rate*weight_vector_gradient 
+            w = w - alpha*w_grad 
             
+            # append new weight vector to weight vector list
             
-        # I wasnt sure if the final SSE was to be computed after the final gradient update, if it is computed the length of the norm_of_weight_vector_gradient will be one less than the length of the Sum_of_Squared_Errors     
+            w_vecs.append(w)
             
-        """   
-        # calculate Sum of Squared Errors for final weight 
+            training_iteration = training_iteration + 1
             
-        Sum_of_Squared_Errors_Current_Weight = 0
-        
-        training_vector_index = 0
-        
-        while(training_vector_index < number_of_training_examples):
-            
-            Sum_of_Squared_Errors_Current_Weight = Sum_of_Squared_Errors_Current_Weight + (target_training_vector[training_vector_index]-np.dot(weight_vector, training_features_matrix[training_vector_index,:]))^2
-            training_vector_index = training_vector_index + 1
-            
-            # append to list of sum of squared errors 
-            
-            Sum_of_Squared_Errors.append(Sum_of_Squared_Errors_Current_Weight)
-        """   
-    
-        return [weight_vector, Sum_of_Squared_Errors, norm_of_weight_vector_gradient]
-
 
 
     
