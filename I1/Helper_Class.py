187f4c4b3a79073901f1b419a655558e13e3436e
mlowell28 <49284072+mlowell28@users.noreply.github.com>
2019-10-18 21:26:20 -0700

    added condition to break loop if gradient too big
    I added a condition to break loop if gradient got too big. It was set to 10^50, I think this is reasonable but a different constant could be used - additionally this could be added as an input argument but I didn't want to break anything.

--- I1/Helper_Class.py
+++ I1/Helper_Class.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+  
+        # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 15 20:26:22 2019
 
@@ -12,12 +13,12 @@ import numpy as np
 
 class Helper_Class:
     
-    def run_gradient_descent(self, y, X, w_0, alpha, lambda_0, epsilon, max_iter):
-        
     # runs gradient descent for linear regression, it accepts a target training vector along with a training features matrix where each row represents a single example,
     # additionally it requires a learning rate, regularization constant and two stopping criteria, norm on the gradient and a maximum number of iterations
     # which ever is reached first terminates the descent and returns a list of the weights as a list of numpy arrays, a list of the gradients as a list of numpy arrays 
     # a list of norms of the gradient
+    
+    def run_gradient_descent(self, y, X, w_0, alpha, lambda_0, epsilon, max_iter):
         
         number_of_training_examples = y.size
         number_of_features = w_0.size
@@ -80,7 +81,7 @@ class Helper_Class:
             
             # exit loop if done
             
-            if(training_iteration > max_iter or np.linalg.norm(w_grad) < epsilon):
+            if(training_iteration > max_iter or np.linalg.norm(w_grad) < epsilon or w_grad_norms[len(w_grad_norms)-1] > 10**50):
                 
                 return [w_vecs, w_grad_vecs, w_grad_norms]
             
@@ -96,11 +97,9 @@ class Helper_Class:
             training_iteration = training_iteration + 1
     
     
-    
-    def calculuate_SSE(self, w_vecs, y, X,):
-        
     # calculuate the SSE between the target vector y, the feature matrix X where the rows of X are the trainining examples and the columns are the features
     # for each weight vector in the list of numpy arrays representing the weight vectors 
+    def calculuate_SSE(self, w_vecs, y, X,):
         
         number_of_w_vecs = len(w_vecs)
         SSE = np.zeros(number_of_w_vecs)
@@ -133,5 +132,8 @@ class Helper_Class:
         
         
         
+            
+
+
             
 
