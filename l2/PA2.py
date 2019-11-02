94ab9615f8560bdd1a0d2dc925136be4b6d9c333
finnagin <flan313@gmail.com>
2019-11-02 15:12:41 -0700

    added plots and argparse and bugfixes

--- l2/PA2.py
+++ l2/PA2.py
@@ -2,6 +2,7 @@ import pandas as pd
 import zipfile as zf
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 
 def loadzip(zipname, csvname, test=False):
@@ -38,15 +39,15 @@ def avg_perceptron_loop(df, iters):
     for i in range(iters):
         w_list.append(w_.copy())
         for idx in range(n):
-            s0 = s
-            s += 1
             if np.dot(X[idx],w)*y[idx] <= 0:
                 w = w + X[idx]*y[idx]
-            w_ = (1/s)*((s0*w_) + w)
+            w_ = ((s*w_) + w)/(s+1)
+            s += 1
     w_list.append(w_.copy())
     return w_list
 
 def kernel_perceptron_loop(df, iters, p):
+    X = np.array(df.values[:,1:])
     y = np.array(df[0].values)
     n = X.shape[0]
     a = np.zeros(n)
@@ -60,6 +61,10 @@ def kernel_perceptron_loop(df, iters, p):
                 a[idx] += 1
     a_list.append(a.copy())
     return a_list
+
+def predictK(a, K, y, X_test):
+    preds = np.sign(np.dot(K,a*y))
+    return preds
 
 
 """
@@ -114,68 +119,124 @@ def kernel_perceptron(df,iters, p):
 """
 
 if __name__ == "__main__":
+parser = argparse.ArgumentParser()
+parser.add_argument("--parts", "-p", type=int, nargs='*', help="The parts you want to run this on seperated by spaces", default=[1, 2, 3])
+parser.add_argument("--hide", action='store_true', help="Add if you want to hide the plots")
+
+args = parser.parse_args()
+if True:
     df = loadzip('data/pa2_train.csv.zip','pa2_train.csv')
     df_val = loadzip('data/pa2_valid.csv.zip','pa2_valid.csv')
     df_test = loadzip('data/pa2_test_no_label.csv.zip','pa2_test_no_label.csv')
     X = np.array(df.values[:,1:])
     y = np.array(df[0].values)
+    n = X.shape[0]
     X_val = np.array(df_val.values[:,1:])
     y_val = np.array(df_val[0].values)
-    X_test = np.array(df_val.values)
-    K_vals = []
+    X_test = np.array(df_test.values)
+    n_test = X_test.shape[0]
+    n_val = X_val.shape[0]
     iters = 15
+
+    
+if 1 in args.parts:
+    print("Starting part 1...")
     w1s = online_perceptron_loop(df, iters)
+    train1 = []
+    val1 = []
+    for w in w1s:
+        tmp_train = 0
+        tmp_val = 0
+        for idx in range(n):
+            tmp_train += int(np.dot(X[idx],w)*y[idx] <= 0)
+        train1.append(tmp_train)
+        for idx in range(n_val):
+            tmp_val += int(np.dot(X_val[idx],w)*y_val[idx] <= 0)
+        val1.append(tmp_val)
+    best_iter = val1.index(min(val1))
+    with open("oplabel.csv","w") as fid:
+        pred2 = -2*((X_test[idx]*w1s[best_iter] <= 0) - .5)
+        for idx in range(n_test):
+            fid.write(str(preds[idx]))
+    if not args.hide:
+        plt.figure()
+        plt.plot(range(len(w2s)),[x/float(n) for x in train1],color="#ff7f00", label="Train")
+        plt.plot(range(len(w2s)),[x/float(n_val) for x in val1],color="#984ea3", label="Validation")
+        plt.legend()
+        plt.show()
+
+if if 2 in args.parts:
+    print("Starting part 2...")
     w2s = avg_perceptron_loop(df, iters)
+    train2 = []
+    val2 = []
+    for w in w2s:
+        tmp_train = 0
+        tmp_val = 0
+        for idx in range(n):
+            tmp_train += int(np.dot(X[idx],w)*y[idx] <= 0)
+        train2.append(tmp_train)
+        for idx in range(n_val):
+            tmp_val += int(np.dot(X_val[idx],w)*y_val[idx] <= 0)
+        val2.append(tmp_val)
+    best_iter = val2.index(min(val2))
+    with open("aplabel.csv","w") as fid:
+        pred2 = -2*((X_test[idx]*w2s[best_iter] <= 0) - .5)
+        for idx in range(n_test):
+            fid.write(str(preds[idx]))
+    if not args.hide:
+        plt.figure()
+        plt.plot(range(len(w2s)),[x/float(n) for x in train2],color="#ff7f00", label="Train")
+        plt.plot(range(len(w2s)),[x/float(n_val) for x in val2],color="#984ea3", label="Validation")
+        plt.legend()
+        plt.show()
+
+
+if 3 in args.parts:
     all_a = []
     Ks = []
-    
-
-    
-
-    train = []
-    val = []
-    for w in w1s:
-        train.append(sum(np.dot(X,w)*y <= 0)/len(y))
-        val.append(sum(np.dot(X_val,w)*y_val <= 0)/len(y_val))
-    plt.figure()
-    plt.plot(range(len(w1s)),train,color="#ff7f00")
-    plt.plot(range(len(w1s)),val,color="#984ea3")
-    plt.show()
-
-    train = []
-    val = []
-    for w in w2s:
-        train.append(sum(np.dot(X,w)*y <= 0)/len(y))
-        val.append(sum(np.dot(X_val,w)*y_val <= 0)/len(y_val))
-    plt.figure()
-    plt.plot(range(len(w2s)),train,color="#ff7f00")
-    plt.plot(range(len(w2s)),val,color="#984ea3")
-    plt.show()
-
-
+    K_vals = []
     for p in [1,2,3,4,5]:
         K = (1+np.matmul(X_val,X.T))**p
         K_vals.append(K)
         K = (1+np.matmul(X,X.T))**p
         Ks.append(K)
-        all_a.append(kernel_perceptron_loop(df,p,iters))
+        all_a.append(kernel_perceptron_loop(df,iters,p))
+
+if 3 in args.parts:
     trains = []
     vals = []
-    for idx in range(len(all_a)):
+    for p in range(len(all_a)):
         train=[]
         val=[]
-        for i in range(len(all_a[idx])):
-            train.append(sum(sum(((all_a[idx][i]*Ks[i].T)*y).T) <= 0)/len(y))
-            val.append(sum(sum(((all_a[idx][i]*K_vals[i].T)*y_val).T) <= 0)/len(y_val))
+        for idx in range(len(all_a[p])):
+            print("a"+str(idx))
+            preds = predictK(all_a[p][idx], Ks[p], y, X)
+            train.append(sum(preds*y<=0))
+            preds = predictK(all_a[p][idx], K_vals[p], y, X_val)
+            val.append(sum(preds*y_val<=0))
         trains.append(train)
         vals.append(val)
-    for train, val in zip(trains, vals):
-        plt.figure()
-        plt.plot(range(len(w1s)),train,color="#ff7f00")
-        plt.plot(range(len(w1s)),val,color="#984ea3")
-        plt.show()
 
-
-
+if 3 in args.parts:
+    p = 1
+    best_iters = [val.index(min([x for x in val])) for val in vals]
+    min_errs = [min([x for x in val]) for val in vals]
+    best_p = min_errs.index(min(min_errs))
+    best_iter = best_iters[best_p]
+    K_test = (1+np.matmul(X_test,X.T))**(best_p+1)
+    preds = predictK(all_a[best_p][best_iter], K_test, y, X_test)
+    with open("kplabel.csv","w") as fid:
+        for idx in range(n_test):
+            fid.write(str(preds[idx]))
+    if not args.hide:
+        for train, val in zip(trains, vals):
+            plt.figure()
+            plt.plot(range(len(w2s)),[1-x/float(n) for x in train],color="#ff7f00", label="Train")
+            plt.plot(range(len(w2s)),[1-x/float(n_val) for x in val],color="#984ea3", label="Validation")
+            plt.legend()
+            plt.title("p="+str(p))
+            plt.show()
+            p+=1
 
 
