b7e9eeeca811ee627387d75640127e87c0639fdd
finnagin <flan313@gmail.com>
2019-11-19 11:34:04 -0800

    updated PA3

--- l3/PA3.py
+++ l3/PA3.py
@@ -5,34 +5,30 @@ import matplotlib.pyplot as plt
 import argparse
 
 
-def loadzip(zipname, csvname, test=False):
+def loadzip(zipname, csvname):
     """
     This function imports a csv from a zipfile and extracts, processes and returns a dataframe from it.
 
     :param zipname: A string containing the path/filename for the zip you want to extract from
     :param csvname: A string containing the filename for the csv file inside the above zip
-    :param test: A boolean indicating if the zip contains the test data
     """
     reader = zf.ZipFile(zipname)
     df = pd.read_csv(reader.open(csvname))
-    ## If not ttest then convert the y values
-    #if not test:
-    #    df[0] = np.where(df[0] == 3, 1,df[0])
-    #    df[0] = np.where(df[0] == 5, -1,df[0])
-    # Add the bias term
-    #df[df.shape[1]] = [1.]*len(df)
     return df
 
 class tree():
-    def __init__(self, df, depth, features = 0):
-        pass
+    def __init__(self, df, max_depth, features = 0):
+        for d in range(max_depth+1):
+            pass
+
+def build_tree(df, max_depth, features = 0):
+    pass
 
 class forrest():
-    def __init__(self):
+    def __init__(self, df, max_depth, features, trees):
         pass
 
-def build_tree(df)
-    pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -44,4 +40,4 @@ if __name__ == "__main__":
     # Load the data from the zips
     df = loadzip('data/pa3_train.zip','pa3_train.csv')
     df_val = loadzip('data/pa3_val.zip','pa3_val.csv')
-    df_test = loadzip('data/pa3_test.zip','pa3_test.csv',True)
+    df_test = loadzip('data/pa3_test.zip','pa3_test.csv')
