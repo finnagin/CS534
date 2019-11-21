33705427fd297464bae19a80047ecc781079e26e
finnagin <flan313@gmail.com>
2019-11-21 00:42:24 -0800

    added predict method

--- l3/PA3.py
+++ l3/PA3.py
@@ -19,31 +19,42 @@ def loadzip(zipname, csvname):
     return df
 
 class node():
-    def __init__(self, indexes, feature_dict):
+    def __init__(self, index, feature_dict):
         self.left = None
         self.right = None
-        self.indexes = indexes
+        self.index = index
         self.feature_dict = feature_dict
+        self.value = None
+        self.feature = None
 
 
 class tree():
     def __init__(self, df, max_depth, n_features = 0):
+        self.y = df['class']
         self.df = df
         self.n_features = n_features
         self.max_depth = max_depth
         root_dict = {}
         for key in df:
-            if len(df[key].unique())>1:
-                root_dict[key] = df[key].unique()
-        self.root = node(list(df.indexes))
+            if len(df[key].unique())>1 and key != "class":
+                root_dict[key] = list(df[key].unique())
+        self.root = node(list(df.index),root_dict)
         root_depth = 0
         self.queue = [(self.root, cur_depth)]
         while len(self.queue) > 0:
             self.split_node(*self.queue.pop(0))
+        del self.df
 
     def split_node(self, node, depth):
         cur_depth = depth + 1
-        if cur_depth > self.max_depth:
+        df_subset = self.df.ix[node.index]
+        df_len = len(df_subset)
+        cur_gini = float(sum(df_subset['class']))/df_len
+        cur_gini = 1-(cur_gini)^2-(1-cur_gini)^2
+        max_gain = 0
+        max_feature = None
+        max_val = None
+        if cur_depth > self.max_depth or cur_gini == 0:
             return
         else:
             if self.n_features > 0:
@@ -51,6 +62,60 @@ class tree():
             else:
                 features = list(node.feature_dict.keys())
             for feature in features:
+                if len(node.feature_dict[feature])==2:
+                    value = node.feature_dict[feature][0]
+                    l_subset = df_subset['class'][df_subset[feature] == value]
+                    r_subset = df_subset['class'][df_subset[feature] != value]
+                    gini_l = float(sum(l_subset))/len(l_subset)
+                    gini_l = 1-gini_l^2-(1-gini_l)^2
+                    gini_r = float(sum(r_subset))/len(r_subset)
+                    gini_r = 1-gini_r^2-(1-gini_r)^2
+                    gain = cur_gini - (float(len(l_subset))/df_len)*gini_l - (float(len(r_subset))/df_len)*gini_r
+                    if gain > max_gain:
+                        max_gain = gain
+                        max_feature = feature
+                        max_val = value
+                else:
+                    for value in node.feature_dict[feature]:
+                        l_subset = df_subset['class'][df_subset[feature] == value]
+                        r_subset = df_subset['class'][df_subset[feature] != value]
+                        gini_l = float(sum(l_subset))/len(l_subset)
+                        gini_l = 1-gini_l^2-(1-gini_l)^2
+                        gini_r = float(sum(r_subset))/len(r_subset)
+                        gini_r = 1-gini_r^2-(1-gini_r)^2
+                        gain = cur_gini - (float(len(l_subset))/df_len)*gini_l - (float(len(r_subset))/df_len)*gini_r
+                        if gain > max_gain:
+                            max_gain = gain
+                            max_feature = feature
+                            max_val = value
+            if max_feature is not None:
+                node.feature = max_feature
+                node.value = max_val
+                new_dict = node.feature_dict.copy()
+                new_dict[max_feature].remove(max_val)
+                if len(new_dict[max_feature]) < 2:
+                    del new_dict[max_feature]
+                l_index = list(df_subset[df_subset[feature] == value].index)
+                r_index = list(df_subset[df_subset[feature] != value].index)
+                node.left = node(l_index,new_dict)
+                node.right = node(r_index,new_dict)
+                self.queue.append((node.left,cur_depth))
+                self.queue.append((node.right,cur_depth))
+
+    def predict(self, df):
+        preds = [None]*len(df)
+        for idx in range(len(df))
+            node = self.root
+            while node.feature is not None:
+                if df[node.feature][idx] == node.value:
+                    node = node.left
+                else:
+                    node = node.right
+            preds[idx] = math.floor(float(sum(self.y.ix[node.index]))/len(self.y.ix[node.index]) + .5)
+        return preds
+
+
+
 
 
 class forrest():
